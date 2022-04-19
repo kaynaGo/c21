d49380887c6d8cf08c9e276f8102ed6128a31fb8
kaynaGo <96072255+kaynaGo@users.noreply.github.com>
2022-04-19 14:08:38 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,84 +1,65 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
 var ball
-var chao
-var w1
-var w2
 var btn1,btn2
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+   
+  var ball_options={
+    restitution: 0.4
+
+  }
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+  
+  ball = Bodies.circle(200,100,20,ball_options)
+  World.add(world,ball)
 
 
-	engine = Engine.create();
-	world = engine.world;
+  btn1 = createImg("right.png")
+  btn1.position(200,30)
+  btn1.size(50,50)
+  btn1.mouseClicked(hForce)
 
-	//Create the Bodies Here.
- var ball_options = {
-	restitution: 0.10
- }
-
- var chao_options = {
-	isStatic :true
- }
+  btn2 = createImg("up.png")
+  btn2.position(20,30)
+  btn2.size(50,50)
+  
+  btn2.mouseClicked(hForce)
  
 
- 
- chao = Bodies.rectangle(400,600,800,20,chao_options)
-World.add(world,chao)
-
-w1 = Bodies.rectangle(790,550,10,80,chao_options)
-World.add(world,chao)
-
-w2 = Bodies.rectangle(620,550,10,80,chao_options)
-World.add(world,chao)
- 
-ball = Bodies.circle(100,200,25,ball_options)
-World.add(world,ball)  
-
-btn1 = createImg("right.png")
-btn1.position(700,30)
-btn1.size(50,50)
-btn1.mouseClicked(ForceX)
-
-btn2 = createImg("up.png")
-btn2.position(20,30)
-btn2.size(50,50)
-btn2.mouseClicked(ForceY)
-}
-
-
-function draw() {
   rectMode(CENTER);
-  background(0);
-  Engine.run(engine);
-  fill("wither")
-  rect(chao.position.x,chao.position.y,800,40);
-  
-  rect(w1.position.x,w1.position.y,10,80);
-
-  ellipse(ball.position.x,ball.position.y,25)
-
-  rect(w2.position.x,w2.position.y,10,80);
-  drawSprites();
-  
- 
+  ellipseMode(RADIUS);
 }
 
-function ForceX(){
-	Matter.Body.applyForce(ball,{x:0,y:0},{x:0.08,y:0})
+function draw() 
+{
+  background(51);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+
+  ellipse(ball.position.x,ball.position.y,20)
 }
 
-function ForceY(){
-	Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.5})
+function hForce(){
+ Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+
 }
-
-
 
